5e8d1444c0c58a2f237d70417e66088fbc21dd8e
mfis <fischermatthias1980@googlemail.com>
2020-09-01 16:41:13 +0200

    Initial Commit

---
+++ iOSHomeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSHomeApp
+//
+//  Created by Matthias Fischer on 01.09.20.
+//  Copyright © 2020 Matthias Fischer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
